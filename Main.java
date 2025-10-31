2922367319cfacdf36f452f12aec3e08be216565
sha614938-alt <sha614938@gmail.com>
2025-10-31 12:20:38 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package CodingPewarisan;
 
 /**
@@ -18,4 +13,5 @@ public class Main {
         // Menampilkan data rekam medis
         rm.infoRekamMedis();
     }
+
 }
